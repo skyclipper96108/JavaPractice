7d61d52795b94e1d60bde563af7a26943854814d
jacob <zhangchujie@inspur.com>
2023-06-07 19:53:20 +0800

    124

---
+++ src/cn/kevin/leetcode/LC053.java
@@ -0,0 +1,13 @@
+package cn.kevin.leetcode;
+
+/**
+ * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ * 子数组 是数组中的一个连续部分。
+ */
+public class LC053 {
+
+    public int maxSubArray(int[] nums) {
+
+    }
+}
