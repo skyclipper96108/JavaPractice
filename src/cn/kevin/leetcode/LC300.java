8bd39360afc045bb7d7b2935bc6c591a85de8dbd
jacob <zhangchujie@inspur.com>
2023-06-09 13:10:09 +0800

    124

---
+++ src/cn/kevin/leetcode/LC300.java
@@ -0,0 +1,56 @@
+package cn.kevin.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+ *
+ * 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+ *
+ */
+public class LC300 {
+
+    public int lengthOfLIS(int[] nums) {
+        int len = nums.length;
+        int[] dp = new int[len];
+        Arrays.fill(dp,1);
+        int res = 0;
+        for(int i=1;i<len;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=Math.max(dp[j]+1,dp[i]);
+                }
+            }
+            System.out.println(Arrays.toString(dp));
+            res = Math.max(res,dp[i]);
+        }
+        return res;
+    }
+
+    /**
+     * 给定两个字符串text1 和text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+     *
+     * 一个字符串的子序列是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+     *
+     * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+     * 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+     *
+     */
+    public int longestCommonSubsequence(String text1, String text2) {
+        int len1 = text1.length();
+        int len2 = text2.length();
+        int[][] dp = new int[len1+1][len2+1];
+        for(int i=1; i<len1+1; i++){
+            for(int j=1;j<len2+1; j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    //如果该位字符相同 最长公共字符串+1
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }else {
+                    dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+}
