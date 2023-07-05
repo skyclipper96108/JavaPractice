be3a31f246be8a1c23195352ae4e4d265b5f39a8
jacob <zhangchujie@inspur.com>
2023-07-05 19:51:40 +0800

    124

--- src/cn/kevin/leetcode/LCEASY.java
+++ src/cn/kevin/leetcode/LCEASY.java
@@ -118,4 +118,27 @@ public class LCEASY {
         System.out.println(Arrays.toString(memo));
         return memo[n];
     }
+
+    /**
+     * 袋子中装有一些物品，每个物品上都标记着数字 1 、0 或 -1 。
+     *
+     * 给你四个非负整数 numOnes 、numZeros 、numNegOnes 和 k 。
+     *
+     * 袋子最初包含：
+     *
+     * numOnes 件标记为 1 的物品。
+     * numZeroes 件标记为 0 的物品。
+     * numNegOnes 件标记为 -1 的物品。
+     * 现计划从这些物品中恰好选出 k 件物品。返回所有可行方案中，物品上所标记数字之和的最大值。
+     *
+     */
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        if(k<=numOnes){
+            return k;
+        }else if(k<=numOnes+numZeros){
+            return numOnes;
+        }else {
+            return 2*numOnes+numZeros-k;
+        }
+    }
 }
