f1cc53d2d505e8ff62c064a4d718fdd0ddab2c90
zhangchujie <jacobzhang96108@outlook.com>
2023-11-15 08:47:31 +0800

    lc

--- src/cn/kevin/leetcode/LCEASY.java
+++ src/cn/kevin/leetcode/LCEASY.java
@@ -195,4 +195,19 @@ public class LCEASY {
         return res%9999;
     }
 
+    /**
+     * 给你一个下标从 0 开始的整数数组 nums 和一个整数 k 。你需要执行以下操作 恰好 k 次，最大化你的得分：
+     *
+     * 从 nums 中选择一个元素 m 。
+     * 将选中的元素 m 从数组中删除。
+     * 将新元素 m + 1 添加到数组中。
+     * 你的得分增加 m 。
+     * 请你返回执行以上操作恰好 k 次后的最大得分。
+     * 2656
+     */
+
+    public int maximizeSum(int[] nums, int k) {
+        int m = Arrays.stream(nums).max().getAsInt();
+        return (2*m+k-1)*k/2;
+    }
 }
