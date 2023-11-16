fda68dd9f698727e6af1424a4fbec2dafe7609eb
zhangchujie <jacobzhang96108@outlook.com>
2023-11-16 09:09:23 +0800

    lc

--- src/cn/kevin/leetcode/LCEASY.java
+++ src/cn/kevin/leetcode/LCEASY.java
@@ -210,4 +210,36 @@ public class LCEASY {
         int m = Arrays.stream(nums).max().getAsInt();
         return (2*m+k-1)*k/2;
     }
+
+    /**
+     * 给你一个下标从 0 开始的整数数组 nums 和一个整数 threshold 。
+     *
+     * 请你从 nums 的子数组中找出以下标 l 开头、下标 r 结尾 (0 <= l <= r < nums.length) 且满足以下条件的 最长子数组 ：
+     *
+     * nums[l] % 2 == 0
+     * 对于范围 [l, r - 1] 内的所有下标 i ，nums[i] % 2 != nums[i + 1] % 2
+     * 对于范围 [l, r] 内的所有下标 i ，nums[i] <= threshold
+     * 以整数形式返回满足题目要求的最长子数组的长度。
+     *
+     * 注意：子数组 是数组中的一个连续非空元素序列。
+     */
+    public int longestAlternatingSubarray(int[] nums, int threshold) {
+        // dp数组 以i开头的满足条件的子数组长度
+        int dp=0; //状态压缩 只需要一组
+        int res=0;
+        int n=nums.length;
+        for (int i=n-1; i>=0; i--){
+            if(nums[i]>threshold) {
+                dp=0;
+            }else if(i==n-1||nums[i]%2!=nums[i+1]%2){
+                dp++;
+            }else {
+                dp=1;
+            }
+            if(nums[i]%2==0){
+                res=Math.max(dp,res);
+            }
+        }
+        return res;
+    }
 }
