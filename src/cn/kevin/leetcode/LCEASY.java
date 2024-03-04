291bb5575e4d21cf124810b533ef927f5f6545eb
zhangchujie <jacobzhang96108@outlook.com>
2024-03-04 09:49:10 +0800

    gp

--- src/cn/kevin/leetcode/LCEASY.java
+++ src/cn/kevin/leetcode/LCEASY.java
@@ -290,8 +290,8 @@ public class LCEASY {
 
     class MyStack {
 
-        Queue<Integer> queue1;
-        Queue<Integer> queue2;
+        private Queue<Integer> queue1;
+        private Queue<Integer> queue2;
         public MyStack() {
             queue1=new LinkedList<>();
             queue2=new LinkedList<>();
@@ -317,6 +317,38 @@ public class LCEASY {
 
         public boolean empty() {
             return queue1.isEmpty();
+        }
+    }
+
+    class MyQueue {
+
+        private Stack<Integer> stack1;
+        private Stack<Integer> stack2;
+        public MyQueue() {
+            stack1=new Stack<>();
+            stack2=new Stack<>();
+        }
+
+        public void push(int x) {
+            while (!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+            stack1.push(x);
+            while (!stack2.isEmpty()){
+                stack1.push(stack2.pop());
+            }
+        }
+
+        public int pop() {
+            return stack1.pop();
+        }
+
+        public int peek() {
+            return stack1.peek();
+        }
+
+        public boolean empty() {
+            return stack1.isEmpty();
         }
     }
 }
