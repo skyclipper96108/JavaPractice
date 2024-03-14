58f5c0be5a50314bc52e48bcd5423ada40fe08af
zhangchujie <jacobzhang96108@outlook.com>
2024-03-14 16:58:35 +0800

    gp

--- src/cn/kevin/leetcode/LCEASY.java
+++ src/cn/kevin/leetcode/LCEASY.java
@@ -445,4 +445,25 @@ public class LCEASY {
             return stack1.isEmpty();
         }
     }
+
+    /**
+     * 给你一个下标从 0 开始、由正整数组成的数组 nums 。
+     *
+     * 你可以在数组上执行下述操作 任意 次：
+     *
+     * 选中一个同时满足 0 <= i < nums.length - 1 和 nums[i] <= nums[i + 1] 的整数 i 。将元素 nums[i + 1] 替换为 nums[i] + nums[i + 1] ，并从数组中删除元素 nums[i] 。
+     * 返回你可以从最终数组中获得的 最大 元素的值。
+     */
+    public long maxArrayValue(int[] nums) {
+        int n = nums.length;
+        long sum = nums[n-1];
+        for(int i=n-2; i>=0; i--){
+            if(nums[i]<=sum){
+                sum+=nums[i];
+            }else{
+                sum=nums[i];
+            }
+        }
+        return sum;
+    }
 }
